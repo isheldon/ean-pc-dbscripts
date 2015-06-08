e1afe1416f920ca14b0b2a166c64ccbcd96f73da
Sheldon Xu <sheldonxu@ebates.com>
2015-06-08 15:41:56 +0800

    optimize ean extra data update script, using multi-thread/process downloading and process

--- EAN_MySQL_refresh_extras.sh
+++ EAN_MySQL_refresh_extras.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-#########################################################################
-## Process Extra files from other public sources                       ##
-## Process tested in MAC OS Mountain Lion                              ##
-## other than the default of the instalation you will need to install: ##
-## -> MAMP regular distribution and defaults.                          ##
-#########################################################################
-# Modified for MAC
 ### Environment ###
 STARTTIME=$(date +%s)
 #
 MYSQL_DIR=/usr/bin/
-# for simplicity I added the MYSQL bin path to the Windows 
-# path environment variable, for Windows set it to ""
-#MYSQL_DIR=""
 #MySQL user, password, host (Server)
 MYSQL_USER=eanuser
 MYSQL_PASS=Passw@rd1
@@ -27,20 +17,49 @@ MYSQL_PORT=3306
 ## directory under HOME_DIR
 FILES_DIR=eanextras
 
+CMD_MYSQL="${MYSQL_DIR}mysql  --local-infile=1 --default-character-set=utf8 --protocol=${MYSQL_PROTOCOL} --port=${MYSQL_PORT} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} --database=${MYSQL_DB}"
+
 ### Import files ###
 ############################################
 # the list should match the tables        ##
 # created by create_ean_extras.sql script ##
 ############################################
-FILES=(
+TABLES=(
 airports
 countries
 regions
+
+openflightsairports
+activepropertybusinessmodel
+propertyidcrossreference
+destinationids
+landmark
+geonames
 )
 
-## home where the process will execute
-## this will be CRONed so it needs the working directory absolute path
-## change to your user home directory
+download_file() {
+    file_url=$1
+    ts_not_change=$(wget -t10 -N --server-response --spider ${file_url} 2>&1 | grep "no newer" | wc -l | tr -d ' ')
+    if [ "${ts_not_change}" -eq 0 ]; then
+        # remote file timestamp changed, download the file
+        axel -n 5 ${file_url}
+    else
+        echo "Remote file not changed, ignore downloading"
+    fi
+}
+
+update_data() {
+    tablename=$1
+    echo "Updating ($MYSQL_DB.$tablename) with REPLACE option..."
+    if [ "$tablename" == "" ]; then
+        $CMD_MYSQL --execute="SET SESSION sql_mode = ''; LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY '\t';"
+    else
+        $CMD_MYSQL --execute="SET SESSION sql_mode = ''; LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES;"
+    fi
+    echo "erasing old records from ($tablename)..."
+    $CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
+}
+
 cd ${HOME_DIR}
 
 echo "Starting at working directory..."
@@ -50,119 +69,65 @@ if [ ! -d ${FILES_DIR} ]; then
    echo "creating download files directory..."
    mkdir ${FILES_DIR}
 fi
-
 ## all clear, move into the working directory
 cd ${FILES_DIR}
 
 ### Download Data ###
-echo "Downloading files using wget..."
-for FILE in ${FILES[@]}
+for TABLE in ${TABLES[@]}
 do
-#   wget  -t 30 --no-check-certificate -nd http://www.ourairports.com/data/$FILE.csv
-    ## download the files via HTTP (no need for https)
-    wget  -t 30 --no-verbose http://www.ourairports.com/data/$FILE.csv
-    ## rename files to CamelCase format
-    mv `echo $FILE | tr \[A-Z\] \[a-z\]`.csv $FILE.txt
+    if [ "${TABLE}" == "airports" ] || [ "${TABLE}" == "countries" ] || [ "${TABLE}" == "regions" ]; then
+        download_file http://www.ourairports.com/data/${TABLE}.csv
+        mv `echo ${TABLE} | tr \[A-Z\] \[a-z\]`.csv ${TABLE}.txt
+    fi
+
+    if [ "${TABLE}" == "openflightsairports" ]; then
+        download_file http://sourceforge.net/p/openflights/code/757/tree/openflights/data/airports.dat?format=raw &
+    fi
+
+    if [ "${TABLE}" == "activepropertybusinessmodel" ]; then
+        download_file http://www.ian.com/affiliatecenter/include/V2/ActivePropertyBusinessModel.zip &
+    fi
+
+    if [ "${TABLE}" == "propertyidcrossreference" ]; then
+        download_file http://www.ian.com/affiliatecenter/include/PropertyID_Cross_Reference_Report.zip &
+    fi
+
+    if [ "${TABLE}" == "destinationids" ]; then
+        download_file http://www.ian.com/affiliatecenter/include/v2/Destination_Detail.zip &
+    fi
+
+    if [ "${TABLE}" == "landmark" ]; then
+        download_file http://www.ian.com/affiliatecenter/include/Landmark.zip &
+    fi
+
+    if [ "${TABLE}" == "geonames" ]; then
+        download_file http://download.geonames.org/export/dump/allCountries.zip &
+    fi
 done
+wait # until all files are downloaded
 echo "downloading files done."
 
-
-### Update MySQL Data ###
-### Parameters that you may need:
-### If you use LOW_PRIORITY, execution of the LOAD DATA statement is delayed until no other clients are reading from the table.
-CMD_MYSQL="${MYSQL_DIR}mysql  --local-infile=1 --default-character-set=utf8 --protocol=${MYSQL_PROTOCOL} --port=${MYSQL_PORT} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} --database=${MYSQL_DB}"
-echo "Uploading Data to MySQL..."
-
-for FILE in ${FILES[@]}
-do
-   ## table name are lowercase
-   tablename=`echo $FILE | tr "[[:upper:]]" "[[:lower:]]"`
-   echo "Uploading ($FILE) to ($MYSQL_DB.$tablename) with REPLACE option..."
-   ## erase all previous data first
-   ## $CMD_MYSQL --execute="TRUNCATE TABLE $FILE;"
-   ## let's try with the REPLACE OPTION
-   $CMD_MYSQL --execute="LOAD DATA LOCAL INFILE '$FILE.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES;"
-   ## we need to erase the records, NOT updated today
-   echo "erasing old records from ($tablename)..."
-   $CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
-done
-echo "Upload done."
-
-## openflights Airports data
-echo -e "\n"
-tablename="openflightsairports"
-echo "Downloading and unzipping (Openflights Airports)..."
-## wget  -t 30 --no-verbose -r -N -nd http://openflights.svn.sourceforge.net/viewvc/openflights/openflights/data/airports.dat
-wget  -t 30 --no-verbose -r -N -nd http://sourceforge.net/p/openflights/code/757/tree/openflights/data/airports.dat?format=raw
+# rename downloaded files to <table_name>.txt
 mv -f airports.dat* openflightsairports.txt
-echo "Uploading (Openflights Airports) to ($MYSQL_DB.$tablename) with REPLACE option..."
-$CMD_MYSQL --execute="LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"';"
-## we need to erase the records, NOT updated today
-echo "erasing old records from ($tablename)..."
-$CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
-
-
-### DOWNLOAD SPECIAL FILES SECTIONS
-echo -e "\n"
-tablename="activepropertybusinessmodel"
-echo "Downloading and unzipping (ActivePropertyBusinessModel)..."
-wget  -t 30 --no-verbose -r -N -nd http://www.ian.com/affiliatecenter/include/V2/ActivePropertyBusinessModel.zip
 unzip -L -o ActivePropertyBusinessModel.zip
-echo "Uploading ($tablename.txt) to ($MYSQL_DB.$tablename) with REPLACE option..."
-$CMD_MYSQL --execute="LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES;"
-## we need to erase the records, NOT updated today
-echo "erasing old records from ($tablename)..."
-$CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
-
-echo -e "\n"
-tablename="propertyidcrossreference"
-echo "Downloading and unzipping (PropertyID Cross Reference Report)..."
-wget  -t 30 --no-verbose -r -N -nd http://www.ian.com/affiliatecenter/include/PropertyID_Cross_Reference_Report.zip
 unzip -L -o PropertyID_Cross_Reference_Report.zip
 mv -f propertyid*.csv propertyidcrossreference.txt
-echo "Uploading (propertyid cross reference report) to ($MYSQL_DB.$tablename) with REPLACE option..."
-$CMD_MYSQL --execute="LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES;"
-## we need to erase the records, NOT updated today
-echo "erasing old records from ($tablename)..."
-$CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
-
-echo -e "\n"
-tablename="destinationids"
-echo "Downloading and unzipping (Destination IDs)..."
-wget  -t 30 --no-verbose -r -N -nd http://www.ian.com/affiliatecenter/include/v2/Destination_Detail.zip
 unzip -L -o Destination_Detail.zip
-### the files are named with the dates, so let's rename it
 mv -f destination_detail*.txt destinationids.txt
-echo "Uploading ($tablename.txt) to ($MYSQL_DB.$tablename) with REPLACE option..."
-$CMD_MYSQL --execute="LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES;"
-## we need to erase the records, NOT updated today
-echo "erasing old records from ($tablename)..."
-$CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
-
-echo -e "\n"
-tablename="landmark"
-echo "Downloading and unzipping (Landmarks IDs)..."
-wget  -t 30 --no-verbose -r -N -nd http://www.ian.com/affiliatecenter/include/Landmark.zip
 unzip -L -o Landmark.zip
-echo "Uploading ($tablename.txt) to ($MYSQL_DB.$tablename) with REPLACE option..."
-$CMD_MYSQL --execute="LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES;"
-## we need to erase the records, NOT updated today
-echo "erasing old records from ($tablename)..."
-$CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
-
-
-## geonames from http://geonames.org
-echo -e "\n"
-tablename="geonames"
-echo "Downloading and unzipping (allCountries from geonames.org)..."
-wget  -t 30 --no-verbose -r -N -nd http://download.geonames.org/export/dump/allCountries.zip
 unzip -L -o allCountries.zip
 mv -f allcountries.txt geonames.txt
-echo "Uploading (allCountries) to ($MYSQL_DB.$tablename) with REPLACE option..."
-$CMD_MYSQL --execute="LOAD DATA LOCAL INFILE '$tablename.txt' REPLACE INTO TABLE $tablename CHARACTER SET utf8 FIELDS TERMINATED BY '\t';"
-## we need to erase the records, NOT updated today
-echo "erasing old records from ($tablename)..."
-$CMD_MYSQL --execute="DELETE FROM $tablename WHERE datediff(TimeStamp, now()) < 0;"
+
+### Update MySQL Data ###
+echo "Uploading Data to MySQL..."
+
+for TABLE in ${TABLES[@]}
+do
+    update_data $TABLE &
+done
+wait # until all data is updated
+echo "Updating done."
+
 
 echo -e "\n"
 echo "Verify database against files..."
@@ -170,34 +135,17 @@ echo "Verify database against files..."
 CMD_MYSQL="${MYSQL_DIR}mysqlshow --count ${MYSQL_DB} --protocol=${MYSQL_PROTOCOL} --port=${MYSQL_PORT} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST}"
 $CMD_MYSQL
 
-FILES=(
-airports
-countries
-regions
-openflightsairports
-activepropertybusinessmodel
-propertyidcrossreference
-propertysuppliermapping
-destinationids
-landmark
-expediaactive
-vacationrentalsactive
-venereactive
-veneredescription
-geonames
-)
-
 ### find the amount of records per datafile
 ### should match to the amount of database records
 echo "+---------------------------------+----------+------------+"
 echo "|             File                |       Records         |"
 echo "+---------------------------------+----------+------------+"
-for FILE in ${FILES[@]}
+for TABLE in ${TABLES[@]}
 do
    ## records=`head --lines=-1 $FILE.txt | wc -l`
    ## To count the number of output records minus the header
-   records=$(($(wc -l $FILE.txt | awk '{print $1}')-1))
-   { printf "|" && printf "%33s" $FILE && printf "|" && printf "%23d" $records && printf "|\n"; }
+   records=$(($(wc -l $TABLE.txt | awk '{print $1}')-1))
+   { printf "|" && printf "%33s" $TABLE && printf "|" && printf "%23d" $records && printf "|\n"; }
 done
 echo "+---------------------------------+----------+------------+"
 echo "Verify done."
@@ -211,3 +159,4 @@ h=$(( secs / 3600 ))
 m=$(( ( secs / 60 ) % 60 ))
 s=$(( secs % 60 ))
 printf "total script time: %02d:%02d:%02d\n" $h $m $s
+
